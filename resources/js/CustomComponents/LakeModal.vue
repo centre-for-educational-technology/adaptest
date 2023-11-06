d5eba59745f36c2dc4a5981712f95c241414145b
GlebRed <glebredko@gmail.com>
2023-11-06 12:23:14 +0000

    References #5, closes #4, user flow for adding observations

---
+++ resources/js/CustomComponents/LakeModal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="modal" :class="{ 'modal-open': showModal }">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">You have chosen <span class="font-bold text-blue-500">{{ selectedAreaName
+            }}</span></h3>
+            <p class="py-4">Now set the marker of your observation by clicking on the map</p>
+            <div class="modal-action">
+                <button class="btn" @click="$emit('update:showModal', false)">
+                    Ok
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue'
+
+// Declare selectedAreaName and showModal as props
+const props = defineProps({
+    selectedAreaName: {
+        type: String,
+        required: true
+    },
+    showModal: {
+        type: Boolean,
+        required: true
+    }
+})
+
+
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -22,95 +22,67 @@
             <!--                        </l-map>-->
 
 
-            <l-map ref="map"
-                   style="height: calc(100vh - 138px); width: 100%;"
-                   :crs="crs"
-                   v-model:zoom="zoom" :useGlobalLeaflet="false"
-                   :center="maaametCenter"
-                   :bounds="bounds"
-                   :maxZoom="14"
-                   :minZoom="3"
-                   :scrollWheelZoom="false"
-            >
+            <l-map ref="map" style="height: calc(100vh - 138px); width: 100%;" :crs="crs" v-model:zoom="zoom"
+                :useGlobalLeaflet="false" :center="maaametCenter" :bounds="bounds" :maxZoom="14" :minZoom="3"
+                :scrollWheelZoom="false" @click="addMarker" @ready="mapReady">
+
 
                 <l-tile-layer
                     url="https://tiles.maaamet.ee/tm/tms/1.0.0/vreljeef/{z}/{x}/{y}.png&ASUTUS=TLU&KESKKOND=ALLIKAD"
-                    :tms="tms"
-                    :full-screen="false"
-                    :worldCopyJump="true"
-                    :z-index="1"
-                    :options="{ maxNativeZoom: 13, maxZoom:13, minZoom: 3 }"
-                />
+                    :tms="tms" :full-screen="false" :worldCopyJump="true" :z-index="1"
+                    :options="{ maxNativeZoom: 13, maxZoom: 13, minZoom: 3 }" />
 
                 <l-tile-layer
                     url="https://tiles.maaamet.ee/tm/tms/1.0.0/hybriid/{z}/{x}/{y}.png&ASUTUS=TLU&KESKKOND=ALLIKAD"
-                    attribution="Maa-ameti kaart, <a href='http://www.maaamet.ee'>Maa-amet</a>"
-                    :tms="tms"
-                    :full-screen="false"
-                    :worldCopyJump="true"
-                    :z-index="2"
-                    :options="{ maxNativeZoom: 13, maxZoom:13, minZoom: 3 }"
-                />
+                    attribution="Maa-ameti kaart, <a href='http://www.maaamet.ee'>Maa-amet</a>" :tms="tms"
+                    :full-screen="false" :worldCopyJump="true" :z-index="2"
+                    :options="{ maxNativeZoom: 13, maxZoom: 13, minZoom: 3 }" />
 
-                <l-geo-json
-                    :visible="geojsonFetched"
-                    :options="{ style: featureStyle, onEachFeature: onEachFeature  }"
+                <l-geo-json :visible="geojsonFetched" :options="{ style: featureStyle, onEachFeature: onEachFeature }"
                     :geojson="geojson"></l-geo-json>
+
+                <!--                <l-marker v-for="marker, index in markers" :lat-lng="marker" @click="removeMarker(index)"></l-marker>-->
+
+                <l-marker v-for="(marker, index) in markers" :key="index" :lat-lng="marker">
+                    <l-popup ref="popup">
+                        Popup content
+                    </l-popup>
+                </l-marker>
+
             </l-map>
+
+
+            <Modal :showModal="showModal" :selectedAreaName="selectedAreaName" @update:showModal="showModal = $event" />
+
+
+
+
+            <!-- Modal for when a GeoJSON layer is not selected -->
+            <div class="modal" :class="{ 'modal-open': showGeoJsonModal }">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg">Please select a GeoJSON layer first</h3>
+                    <div class="modal-action">
+                        <button class="btn" @click="toggleGeoJsonModal">
+                            Ok
+                        </button>
+                    </div>
+                </div>
+            </div>
+
 
         </main>
     </AppLayout>
 </template>
 
 <script setup>
-import {LGeoJson, LMap, LTileLayer} from "@vue-leaflet/vue-leaflet";
+import { LGeoJson, LIcon, LMap, LMarker, LPopup, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import "leaflet/dist/leaflet.css";
-import {onMounted, ref, watch} from 'vue';
-import {latLng, latLngBounds} from "leaflet/dist/leaflet-src.esm.js";
-import {relief_layers} from "@/constants.js";
+import { nextTick, onMounted, ref, watch } from 'vue';
+import { latLng, latLngBounds } from "leaflet/dist/leaflet-src.esm.js";
 import * as L from 'leaflet';
 import 'proj4leaflet';
-
-function featureStyle(feature) {
-    const color = feature.properties && feature.properties.color ? feature.properties.color : 'blue';
-
-    return {
-        fillColor: color,
-        color: 'white',
-        weight: 2,
-        opacity: 1,
-        fillOpacity: 0.7,
-    };
-}
-
-let geojsonFetched = ref(false);
-
-const onEachFeature = (feature, layer) => {
-    const tooltip = layer.bindTooltip(feature.properties.nimi, {
-        permanent: false,
-        direction: "center",
-        className: "my-tooltip",
-    });
-
-    layer.on("click", (e) => {
-
-        tooltip.openTooltip();
-
-    });
-};
-
-// let zoom = ref(6);
-let tms = ref(true);
-let maaametCenter = latLng(58.379, 24.554)
-//
-let bounds = latLngBounds(
-    [60.4349, 29.4338],
-    [56.7458, 20.373]
-);
-
-let tilelayers = ref(relief_layers);
-
+import Modal from "@/CustomComponents/LakeModal.vue";
 
 const map = ref(null);
 const zoom = ref(4);
@@ -124,6 +96,146 @@ const keyboard = ref(false);
 const zoomControl = ref(true);
 const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+
+let selectedAreaName = ref('');
+let showGeoJsonModal = ref(false);
+let showModal = ref(false);
+
+function toggleGeoJsonModal() {
+    showGeoJsonModal.value = !showGeoJsonModal.value;
+}
+
+
+function featureStyle(feature) {
+    const color = feature.properties && feature.properties.color ? feature.properties.color : 'blue';
+
+    return {
+        fillColor: color,
+        color: 'white',
+        weight: 2,
+        opacity: 1,
+        fillOpacity: 0.7,
+    };
+}
+
+
+
+let geojsonFetched = ref(false);
+let selectedLayer = null;
+
+
+const onEachFeature = (feature, layer) => {
+    layer.on('click', () => {
+
+        // Reset the style of the previously selected layer
+        if (selectedLayer) {
+            selectedLayer.setStyle({
+                weight: 3, // Set the border weight back to normal
+                color: '#3388ff'
+
+            });
+        }
+
+
+        selectedAreaName = feature.properties.nimi;
+
+        layer.setStyle({
+            weight: 5, // Make the border of the polygon bolder
+            //change the color of the border to red
+            color: 'red',
+        });
+
+
+        showModal.value = true;
+        console.log('show modal')
+        selectedLayer = layer;
+        tooltip.openTooltip();
+    });
+
+    const tooltip = layer.bindTooltip(feature.properties.nimi, {
+        permanent: false,
+        direction: "center",
+        className: "my-tooltip",
+    });
+
+
+
+};
+
+// let zoom = ref(6);
+let tms = ref(true);
+let maaametCenter = latLng(58.379, 24.554)
+//
+let bounds = latLngBounds(
+    [60.4349, 29.4338],
+    [56.7458, 20.373]
+);
+
+
+let markers = []
+let mapInstance = null;
+
+
+function mapReady() {
+    console.log('map ready');
+    mapInstance = map.value.leafletObject;
+}
+
+function addMarker(event) {
+    if (!selectedLayer) {
+
+        showGeoJsonModal.value = true;
+        return;
+    }
+    //if below is geo-json layer, then do not add marker
+    if (event.originalEvent.target.classList.contains('leaflet-interactive')) {
+        return;
+    }
+
+    const latlng = event.latlng;
+    const marker = L.marker(latlng);
+    const popup = L.popup().setContent(`
+      <p class="mb-2 text-lg font-semibold text-gray-900">Add new observation?</p>
+      <div class="flex space-x-2">
+        <button id="yesButton" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Yes</button>
+        <button id="noButton" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">No</button>
+      </div>
+    `);
+    marker.bindPopup(popup);
+
+    let observationAdded = false;
+
+    nextTick(() => {
+        mapInstance.invalidateSize();
+        mapInstance.addLayer(marker);
+        marker.openPopup(); // Open the popup
+
+
+        // Add click event listener to the "Yes" button
+        const yesButton = document.getElementById('yesButton');
+        yesButton.addEventListener('click', () => {
+            observationAdded = true;
+            markers.push(event.latlng);
+        });
+
+
+        // Add click event listener to the "No" button
+        const noButton = document.getElementById('noButton');
+        noButton.addEventListener('click', () => {
+            mapInstance.removeLayer(marker);
+
+        });
+
+        // Add popupclose event listener to the map
+        mapInstance.on('popupclose', () => {
+            mapInstance.removeLayer(marker);
+
+        });
+    });
+
+
+}
+
 
 
 // let projection = new L.Proj.CRS('EPSG:3301', '+proj=lcc +lat_1=59.33333333333334 +lat_2=58 +lat_0=57.51755393055556 +lon_0=24 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs', {
@@ -163,8 +275,8 @@ onMounted(() => {
             //transform coordinates
             geojson = data;
         }).then(() => {
-        geojsonFetched.value = true;
-    })
+            geojsonFetched.value = true;
+        })
 });
 
 if (navigator.geolocation) {
@@ -182,6 +294,11 @@ watch([center, zoom], ([newCenter, newZoom]) => {
         map.value = { center: newCenter, zoom: newZoom };
     }
 });
+
+
+const addObservation = (event) => {
+
+};
 
 
 </script>
