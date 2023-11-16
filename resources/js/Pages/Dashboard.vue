5b252cdbf0b5c49576d49631cdac5c4e4e523514
GlebRed <glebredko@gmail.com>
2023-11-16 13:28:52 +0000

    Wip

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -30,7 +30,7 @@
 
             <l-map ref="map" :crs="crs" v-model:zoom="zoom"
                    :useGlobalLeaflet="false" :center="maaametCenter" :bounds="bounds" :maxZoom="14" :minZoom="3"
-                   :scrollWheelZoom="false" @click="addMarker">
+                   :scrollWheelZoom="false" @click="addMarker" @ready="mapReady">
 
 
                 <l-tile-layer
@@ -296,7 +296,7 @@ onMounted(() => {
         }).then(() => {
         geojsonFetched.value = true;
     })
-    mapReady();
+    mapLoaded = true;
 });
 
 if (navigator.geolocation) {
