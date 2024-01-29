80f53f82eb41042099404feccc6e08d7d86559e8
GlebRed <glebredko@gmail.com>
2024-01-29 12:06:31 +0000

    Fixes

--- database/seeders/WaterBodiesSeeder.php
+++ database/seeders/WaterBodiesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class WaterBodiesSeeder extends Seeder
 {
@@ -13,29 +14,36 @@ class WaterBodiesSeeder extends Seeder
         $data = json_decode($geojson, true);
 
         // Loop through the array, and for each item, create a new row in the Water bodies table
-        foreach ($data['features'] as $feature) {
-            DB::table('water_bodies')->insert([
-                'title' => $feature['properties']['nimi'],
-                'code' => $feature['properties']['kr_kood'],
-                'type' => $feature['properties']['tyyp'],
-                'area' => $feature['properties']['pindala'],
-                'sys_id' => $feature['properties']['sys_id'],
-            ]);
+        foreach ($data['features'] as $index => $feature) {
+            try {
+                DB::table('water_bodies')->insert([
+                    'title' => $feature['properties']['nimi'],
+                    'code' => $feature['properties']['kr_kood'],
+                    'type' => $feature['properties']['tyyp'],
+                    'area' => $feature['properties']['pindala'],
+                    'sys_id' => $feature['properties']['sys_id'],
+                ]);
+            } catch (\Exception $e) {
+                Log::error("Error inserting feature at index {$index}: {$e->getMessage()}");
+            }
         }
-
 
         $geojson = file_get_contents(public_path('geojson/vooluvesi.json'));
         $data = json_decode($geojson, true);
 
         // Loop through the array, and for each item, create a new row in the Water bodies table
-        foreach ($data['features'] as $feature) {
-            DB::table('water_bodies')->insert([
-                'title' => $feature['properties']['nimi'],
-                'code' => $feature['properties']['kr_kood'],
-                'type' => $feature['properties']['tyyp'],
-                'watershed' => $feature['properties']['valgala'],
-                'sys_id' => $feature['properties']['sys_id'],
-            ]);
+        foreach ($data['features'] as $index => $feature) {
+            try {
+                DB::table('water_bodies')->insert([
+                    'title' => $feature['properties']['nimi'],
+                    'code' => $feature['properties']['kr_kood'],
+                    'type' => $feature['properties']['tyyp'],
+                    'watershed' => $feature['properties']['valgala'],
+                    'sys_id' => $feature['properties']['sys_id'],
+                ]);
+            } catch (\Exception $e) {
+                Log::error("Error inserting feature at index {$index}: {$e->getMessage()}");
+            }
         }
     }
 }
